f0a164c144158036e08ed81b006229ab1a0dc9ab
Mthobeki <mthobekisikhosang@gmail.com>
2023-09-22 11:58:06 +0200

    my files

--- main.c
+++ main.c
@@ -1,41 +1,56 @@
-#define _GNU_SOURCE // Required for getline in some systems
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    char *content = NULL;
-    FILE *file;
-    size_t size = 0;
-    ssize_t read_line = 1;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
+/* Define the stack_t type */
+typedef struct stack_t {
+    /* Define the structure members as needed */
+    int data; /* Example member */
+    struct stack_t *next;
+} stack_t;
 
-    if (argc != 2)
-    {
+/* Define the bus_t type */
+typedef struct bus_t {
+    FILE *file;
+    char *content;
+    /* Define other members as needed */
+} bus_t;
+
+bus_t bus = {NULL, NULL}; /* Initialize the bus variable */
+
+/* Define the free_stack function */
+void free_stack(void) {
+    /* Implement the function as needed */
+    /* You should free memory associated with the stack elements here */
+}
+
+/* Define the execute function without the unused 'content' parameter */
+void execute(void) {
+    /* Implement the function as needed */
+    /* You can access 'bus.content' to use the content if necessary */
+}
+
+int main(int argc, char *argv[]) {
+    char content[1024]; /* Assuming a maximum line length of 1024 */
+    FILE *file;
+
+    if (argc != 2) {
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
+
     file = fopen(argv[1], "r");
-    if (!file)
-    {
+    if (!file) {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
-    while (read_line > 0)
-    {
-        content = NULL;
-        read_line = getline(&content, &size, file);
+
+    while (fgets(content, sizeof(content), file) != NULL) {
         bus.content = content;
-        counter++;
-        if (read_line > 0)
-        {
-            // Execute code here
-        }
-        free(content);
+        execute(); /* Call the function without the unused parameter */
     }
-    free_stack(stack);
+
     fclose(file);
-    return (0);
+    free_stack();
+    return 0;
 }
 
